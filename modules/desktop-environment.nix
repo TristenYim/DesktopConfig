fc65f4ce43e338c34b3695a4b539d18f483627da
TristenYim <unfathomy@proton.me>
2024-09-17 18:23:57 -0700

    Enabled running Electron apps with native Wayland.
    This is done with the environment variable "NIXOS_OZONE_WL=1"

--- modules/desktop-environment.nix
+++ modules/desktop-environment.nix
@@ -74,6 +74,7 @@
                 enable = true;
                 package = inputs.hyprland.packages."${pkgs.system}".hyprland;
             };
+            environment.sessionVariables.NIXOS_OZONE_WL = "1";
         })
 
         # Nerd Fonts
