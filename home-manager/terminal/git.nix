f59d26dcba5521cc3fd67ce797e51cc3b3bf4cb1
TristenYim <unfathomy@proton.me>
2024-11-20 23:49:03 -0800

    Enabled lazygit.
    Lazygit is used to view and manage git diffs using a terminal UI.

--- home-manager/terminal/git.nix
+++ home-manager/terminal/git.nix
@@ -1,24 +1,31 @@
-{ config, pkgs, lib, ... }: {
+{ config, lib, ... }: {
 
     # Set a toggle to enable git
     options = {
         git-home.enable = lib.mkEnableOption "Enables git with Home Manager";
+        lazygit-home.enable = lib.mkEnableOption "Enables git with Home Manager";
     };
  
-    config = lib.mkIf config.git-home.enable 
-    {
-        programs.git = {
-            enable = true;
-            userEmail = "unfathomy@proton.me";
-            userName = "TristenYim";
-            extraConfig = {
-                safe = {
-                    directory = [
-                        "/etc/nixos"
-                        "/media/Hdd/SchoolNotes"
-                    ];
+    config = lib.mkMerge
+    [
+        (lib.mkIf config.git-home.enable {
+            programs.git = {
+                enable = true;
+                userEmail = "unfathomy@proton.me";
+                userName = "TristenYim";
+                extraConfig = {
+                    safe = {
+                        directory = [
+                            "/etc/nixos"
+                            "/media/Hdd/SchoolNotes"
+                        ];
+                    };
                 };
             };
-        };
-    };
+            lazygit-home.enable = lib.mkDefault true;
+        })
+        (lib.mkIf config.lazygit-home.enable {
+            programs.lazygit.enable = true;
+        })
+    ];
 }
