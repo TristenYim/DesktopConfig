ce8a3e42f88d3cd27ea84b855ad5cb87153f7e60
TristenYim <unfathomy@proton.me>
2025-03-20 17:13:59 -0700

    Removed split preservation in dwindle layout
    Split preservation was unnecessarily confusing and inconsistent.

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -131,7 +131,6 @@ in {
                 # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
                 dwindle = {
                     pseudotile = true; # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-                    preserve_split = true; # you probably want this
                 };
 
                 # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
